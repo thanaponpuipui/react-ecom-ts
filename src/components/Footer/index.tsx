16f6fb5a1feaecde30ee9a00cf2776d12b6997ad
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-10 19:03:15 +0700

    footer positioning

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './Footer.scss'
 
+type socialMediaLink = {
+  link: string
+  media: string
+}
+
 const Footer = () => {
+
+  const [sm, setSm] = useState<socialMediaLink[]>([])
+
+  function testSm () {
+    setSm([
+      {
+        link: '/',
+        media: 'facebook'
+      }
+    ])
+  }
+
+  useEffect(() => {
+    testSm()
+  }, [])
+
   return (
     <div className='footer'>
-      test
+      <div className='infomation'>
+        <h2>INFOMATION</h2>
+        <ul className='info-list'>
+          <li>Contact Us</li>
+          <li>Contact Us</li>
+          <li>Contact Us</li>
+          <li>Contact Us</li>
+        </ul>
+      </div>
+
+      <div className='social-list'>
+        {
+          sm.map((i, index) => {
+            return <span key={index}>{i['media']}</span>
+          })
+        }
+      </div>
     </div>
   )
 }
