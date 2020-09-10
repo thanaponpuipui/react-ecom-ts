2927d656537a15fac064110e64eac3ab46bc3aa5
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-10 14:11:18 +0700

    test footer and position

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import './Footer.scss'
 
 const Footer = () => {
   return (
-    <>
-
-    </>
+    <div className='footer'>
+      test
+    </div>
   )
 }
 
--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -7,7 +7,9 @@ const HomePage = () => {
   return (
     <>
       <Header/>
-
+        <img src='https://ucarecdn.com/2e5462d7-e919-44e1-b02b-e328cab6a863/1598250920D9Kef.jpg' alt='test banner'
+          style={{width: '100%'}}
+        />
       <Footer/>
     </>
   )
