2edb8f253abfd7b73365cac4a302151488e80ffc
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-12 22:10:40 +0700

    dynamically change site title

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.scss';
 import { Route } from 'react-router-dom'
 
 import HomePage from './pages/HomePage';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Batadee - main'
+  }, [])
+
   return (
     <div className='App'>
       <Route exact path='/' component={HomePage}/>
