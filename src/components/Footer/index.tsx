4c80b389c8c283fe48e178afba78198ec79917fe
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-13 16:46:14 +0700

    footer s Icon test

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -7,18 +7,37 @@ type socialMediaLink = {
   media: string
 }
 
+
+const mockLink = [
+  {
+    link: '#',
+    media: 'facebook'
+  },
+  {
+    link: '#',
+    media: 'shopee'
+  },
+  {
+    link: '#',
+    media: 'instagram'
+  },
+  {
+    link: '#',
+    media: 'twitter'
+  },
+  {
+    link: '#',
+    media: 'youtube'
+  }
+]
+
 const Footer = () => {
 
   const [sm, setSm] = useState<socialMediaLink[]>([])
 
   // fetch list from admin page
   function testSm () {
-    setSm([
-      {
-        link: '/',
-        media: 'facebook'
-      }
-    ])
+    setSm(mockLink)
   }
 
   useEffect(() => {
@@ -30,11 +49,14 @@ const Footer = () => {
       <div className='infomation'>
         <h2>INFOMATION</h2>
         <ul className='info-list'>
-          <li className='info'>Contact Us</li>
-          <li className='info'>About Us</li>
-          <li className='info'></li>
-          <li className='info'>Contact Us</li>
+          <li className='info'>Contact us</li>
+          <li className='info'>About us</li>
+          <li className='info'>Job opportunities</li>
+          <li className='info'>Sell with us</li>
+          <li className='info'>Sitemap</li>
+          <li className='info'>FAQs</li>
         </ul>
+        <span className='cp'>Copyright © 2020 ABCDEFG. All rights reserved.</span>
       </div>
 
       <div className='social-list'>
@@ -43,7 +65,7 @@ const Footer = () => {
             const facebookIcon = 'https://ucarecdn.com/59b6feb1-9b17-4d5e-a3c8-9ea86121d325/facebook.png'
             const twitterIcon = 'https://ucarecdn.com/59bb25a8-8ff5-44ff-82d7-29febd5e3f45/twitter.png'
             const instagramIcon = 'https://ucarecdn.com/bd6ef5c3-1135-48f4-99c6-db680a227bce/instagramsketched.png'
-            const shopeeIcon = 'https://ucarecdn.com/de2e62d7-d079-4b3c-b785-2535c64f9a7b/kisspngshopeeindonesiaonlineshoppingproductbukalapakitgaragetruegamingstore5c02f063ce93476779180715436964838461.png'
+            const shopeeIcon = 'https://ucarecdn.com/07a71030-35a2-421a-a055-245ae1c64a6d/shopee64.png'
             const youtubeIcon = 'https://ucarecdn.com/e38515d5-ec10-4406-b562-f3b6351c3b61/youtube.png'
             let icon;
             switch (i.media) {
@@ -66,7 +88,7 @@ const Footer = () => {
                 break
             }
             return (
-              <div className="image-wrapper">
+              <div className="image-wrapper" key={index}>
                 <Link to={i.link}>
                   <img src={icon} alt={i.media}/>
                 </Link>
