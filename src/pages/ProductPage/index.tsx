a369ce008dc6aa1e1ee4da03f7359be760b1ba63
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-13 11:54:28 +0700

    start product page

---
+++ src/pages/ProductPage/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProductPage = () => {
+  return (
+    <>
+      {/* top banner or navbar */}
+      {/* side box easy access tool (easy finder panel) */}
+      {/* product items */}
+      {/* same footer with homepage */}
+    </>
+  )
+}
+
+export default ProductPage
