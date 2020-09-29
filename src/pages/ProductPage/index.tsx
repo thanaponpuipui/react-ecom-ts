7420154d1c0b2f6253afc3e3aa53476b3a59f7a4
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-29 13:24:27 +0700

    product layout

--- src/pages/ProductPage/index.tsx
+++ src/pages/ProductPage/index.tsx
@@ -1,12 +1,33 @@
 import React from 'react'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import ProductCard from '../../components/ProductCard'
+import './ProductPage.scss'
 
 const ProductPage = () => {
   return (
     <>
-      {/* top banner or navbar */}
-      {/* side box easy access tool (easy finder panel) */}
-      {/* product items */}
-      {/* same footer with homepage */}
+      <Header />
+
+      <main className="product-body">
+        <div className="side-panel"></div>
+        <div className="content">
+          <h2 className='title'>สินค้า</h2>
+          <div className="item-list">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
     </>
   )
 }
