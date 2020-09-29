587dd11596320c20c65ca4e43e0a870b30d40df0
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-29 15:53:38 +0700

    title dynamic to page

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import React, { useEffect } from 'react'
+import React from 'react'
 import './App.scss';
 import { Route, Switch } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import ProductPage from './pages/ProductPage'
+import ProductBrand from './pages/ProductBrand'
 
 function App() {
-  useEffect(() => {
-    document.title = 'Bathadee - main'
-  }, [])
 
   return (
     <div className='App'>
       <Switch>
         <Route exact path='/' component={HomePage}/>
-        <Route path='/product/:g/:cat' component={ProductPage}/>
+        <Route path='/product/:filter/:cat' component={ProductPage}/>
+        <Route path='/product-brand/:brand' component={ProductBrand}/>
       </Switch>
     </div>
   );
