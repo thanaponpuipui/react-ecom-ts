07b64b0100ffca4d37a7b5d38b9f1fc43dd61f6d
Thanapon Teerakaisri <Thanaponpuipui@gmail.com>
2020-09-29 19:42:18 +0700

    product Brand page add brand show

--- src/pages/ProductBrand/index.tsx
+++ src/pages/ProductBrand/index.tsx
@@ -1,12 +1,39 @@
 import React from 'react'
+import Footer from '../../components/Footer'
+import Header from '../../components/Header'
+import ProductCard from '../../components/ProductCard'
+import ProductFilter from '../../components/ProductFilter'
+import './ProductBrandPage.scss'
 
-const ProductBrand = () => {
+const ProductBrandPage = () => {
   return (
-
     <>
-      product brand
+      <Header />
+
+      <section className="brand-show">
+        Brand Logo and details
+      </section>
+
+      <main className="product-body">
+        <div className="side-panel">
+          <ProductFilter/>
+        </div>
+        <div className="content">
+          <h2 className='title'>สินค้า</h2>
+          <div className="item-list">
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+            <ProductCard/>
+          </div>
+        </div>
+      </main>
+
+      <Footer />
     </>
   )
 }
 
-export default ProductBrand
+export default ProductBrandPage
+
